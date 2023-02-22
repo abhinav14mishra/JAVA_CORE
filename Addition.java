136d52fab349785a208b0af8b27d086de8417d5c
abhinav14mishra <abhinav14mishra@gmail.com>
2023-02-22 20:32:32 +0530

    First

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main(String[] args)
+	{   
+		int num1=20;
+		int num2=20;
+		int num3=20;
+		int num4=20;
+		int result=num1+num2+num3+num4;
+		System.out.println("The addition of 4 number is = ");
+		System.out.println(result);
+	}
+}
